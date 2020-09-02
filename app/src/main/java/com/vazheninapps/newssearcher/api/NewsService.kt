3aa11b1cb3ee6a23b1320dd840894dbd0d85a2d5
Andrey <vazh2100@list.ru>
2020-09-02 21:53:00 +0300

    create NewsService.kt

--- app/src/main/java/com/vazheninapps/newssearcher/api/NewsService.kt
+++ app/src/main/java/com/vazheninapps/newssearcher/api/NewsService.kt
@@ -10,15 +10,26 @@ import retrofit2.http.Query
 
 interface NewsService {
 
-    @Headers("Authorization: $apiKey")
+    @Headers(HEADER_AUTHORIZATION)
     @GET("everything")
-    fun getNews(@Query("q")  q:String = "android"): Single<Response>
+    fun getNews(
+        @Query(QUERY_Q) q: String,
+        @Query(QUERY_LANGUAGE) language: String,
+        @Query(QUERY_PAGE_SIZE) pageSize: Int = 10,
+        @Query(QUERY_PAGE) page: Int = 1
+    ): Single<Response>
 
-    companion object{
-       const val apiKey = "e7a4d3493ec84a1a9232789bf7a943cf"
+    companion object {
+        private const val API_KEY = "e7a4d3493ec84a1a9232789bf7a943cf"
+
+        private const val HEADER_AUTHORIZATION = "Authorization: $API_KEY"
+
+        private const val QUERY_Q = "q"
+        private const val QUERY_LANGUAGE = "language"
+        private const val QUERY_PAGE_SIZE = "pageSize"
+        private const val QUERY_PAGE = "page"
 
     }
-
 
 
 }
