1ae8a7fd64a5d9254d94e542e40b6b83197f5fc6
Andrey <vazh2100@list.ru>
2020-09-04 07:09:08 +0300

    add method for checking internet

---
+++ app/src/main/java/com/vazheninapps/newssearcher/utils/NetUtils.kt
@@ -0,0 +1,24 @@
+package com.vazheninapps.newssearcher.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+
+object NetUtils {
+
+    fun isInternetConnection(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val networkCapabilities = connectivityManager.activeNetwork ?: return false
+            val actNw = connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+            actNw.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        } else {
+            @Suppress("DEPRECATION")
+            val networkInfo = connectivityManager.activeNetworkInfo
+            @Suppress("DEPRECATION")
+            networkInfo != null && networkInfo.isConnected
+        }
+}}
